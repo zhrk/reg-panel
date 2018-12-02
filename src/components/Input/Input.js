81e6eab164f1cffb5f56ec25086dfd87309b30a7
Zakhar Shchipets <zshchipets@gmail.com>
2018-12-02 17:00:28 +0300

    one source of truth

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,10 +3,30 @@ import './Input.css';
 
 class Input extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="input">
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
