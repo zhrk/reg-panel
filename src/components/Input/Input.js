1a285ad32fa9f853bfb55b460f3c2c8ac47b5d16
Zakhar Shchipets <zshchipets@gmail.com>
2018-12-02 16:40:46 +0300

    + Input

---
+++ src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Input.css';
+
+class Input extends Component {
+
+  render() {
+    return (
+      <div className="input">
+        <input type="text" />
+      </div>
+    );
+  }
+  
+}
+
+export default Input;
--- src/components/StepOne/StepOne.js
+++ src/components/StepOne/StepOne.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './StepOne.css';
 import Title from '../Title/Title';
 import SubTitle from '../SubTitle/SubTitle';
+import Input from '../Input/Input';
 
 const StepOne = () => {
   return (
@@ -12,7 +13,7 @@ const StepOne = () => {
       />
       <SubTitle text="Пройдите быструю регистрацию для оформления покупки франшизы" />
       <form className="step-one__form">
-        123
+        <Input />
       </form>
     </div>
   );
